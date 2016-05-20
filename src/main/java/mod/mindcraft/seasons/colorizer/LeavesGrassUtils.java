0a55d8b392e6b2bcc902146b2bfac954411ed404
EdwinMindcraft <edwin.mindcraft@gmail.com>
2016-05-20 19:15:00 +0200

    Biomes O'Plenty Colorizers

--- src/main/java/mod/mindcraft/seasons/colorizer/LeavesGrassUtils.java
+++ src/main/java/mod/mindcraft/seasons/colorizer/LeavesGrassUtils.java
@@ -3,6 +3,12 @@ package mod.mindcraft.seasons.colorizer;
 import java.util.ArrayList;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import biomesoplenty.api.block.BOPBlocks;
+import biomesoplenty.common.block.BlockBOPLeaves;
+import biomesoplenty.common.enums.BOPTrees;
 import mod.mindcraft.seasons.Seasons;
 import mod.mindcraft.seasons.WorldInterface;
 import mod.mindcraft.seasons.api.init.SeasonsAPI;
@@ -19,6 +25,7 @@ import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ColorizerFoliage;
 import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.registry.RegistryDelegate;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
@@ -44,7 +51,23 @@ public class LeavesGrassUtils {
                     return ColorizerUtils.mix(originalColor, new ISeasonColorizer.Wrapper(new FoliageSeasonColorizer()).getColor(iblockstate, pos, worldInterface.getWorld().getWorldTime(), worldInterface.getWorld()), 0.33F);
 				}
 			}
-			
+			else if (Loader.isModLoaded("BiomesOPlenty")) {
+				if (iblockstate.getBlock().equals(BOPBlocks.leaves_0) || iblockstate.getBlock().equals(BOPBlocks.leaves_2)) {
+					return originalColor;
+				}
+				else if (iblockstate.getBlock().equals(BOPBlocks.leaves_1)) {
+					BOPTrees treeType = iblockstate.getValue(BlockBOPLeaves.paging.getVariantProperty(1));
+					if (!treeType.equals(BOPTrees.FIR)) {
+						return originalColor;
+					}
+				}
+				else if (iblockstate.getBlock().equals(BOPBlocks.leaves_3)) {
+					BOPTrees treeType = iblockstate.getValue(BlockBOPLeaves.paging.getVariantProperty(3));
+					if (treeType.equals(BOPTrees.HELLBARK) || treeType.equals(BOPTrees.JACARANDA)) {
+						return originalColor;
+					}
+				}
+			}
             return ColorizerUtils.mix(originalColor, new ISeasonColorizer.Wrapper(new FoliageSeasonColorizer()).getColor(iblockstate, pos, worldInterface.getWorld().getWorldTime(), worldInterface.getWorld()), 0.25F);        	
 		} catch (NullPointerException e) {
 			return originalColor;
@@ -67,12 +90,30 @@ public class LeavesGrassUtils {
 		Map<RegistryDelegate<Block>, IBlockColor> map = ReflectionHelper.getPrivateValue(BlockColors.class, colors, "blockColorMap");
 		ArrayList<Block> leavesBlock = new ArrayList<Block>();
 		ArrayList<Block> grassBlock = new ArrayList<Block>();
+		Logger logger = LogManager.getLogger("Color Modifier");
+		logger.info("Applying Vanilla Color Modification...");
 		leavesBlock.add(Blocks.LEAVES);
 		leavesBlock.add(Blocks.LEAVES2);
 		leavesBlock.add(Blocks.VINE);
 		grassBlock.add(Blocks.DOUBLE_PLANT);
 		grassBlock.add(Blocks.GRASS);
 		grassBlock.add(Blocks.TALLGRASS);
+		if (Loader.isModLoaded("BiomesOPlenty")) {
+			logger.info("Applying Biomes O\' Plenty Color Modification...");
+			leavesBlock.add(BOPBlocks.leaves_0);
+			leavesBlock.add(BOPBlocks.leaves_1);
+			leavesBlock.add(BOPBlocks.leaves_2);
+			leavesBlock.add(BOPBlocks.leaves_3);
+			leavesBlock.add(BOPBlocks.leaves_4);
+			leavesBlock.add(BOPBlocks.leaves_5);
+			leavesBlock.add(BOPBlocks.leaves_6);
+			
+			grassBlock.add(BOPBlocks.plant_0);
+			grassBlock.add(BOPBlocks.plant_1);
+			grassBlock.add(BOPBlocks.double_plant);
+			grassBlock.add(BOPBlocks.grass);
+			grassBlock.add(BOPBlocks.ivy);
+		}
 		for(Block b : leavesBlock) {
 			IBlockColor color = map.get(b.delegate);
 			if(color != null)
